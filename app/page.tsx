272bcb1a6260e5f7aced56a3af63ec9e090879b6
omega1234az <supakit0920@gmail.com>
2025-01-07 10:40:41 +0700

    ไก่ทอด

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    <>
     <h1> หิวข้าว </h1>
+    <div className="text-3xl">
+          SE66 Team20 ไก่ทอด
+    </div>
+    </>
   );
 }
