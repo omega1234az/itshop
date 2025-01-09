411cc2db1e86b3314db528a8cab94ca0fe0cab69
omega1234az <supakit0920@gmail.com>
2025-01-09 13:31:40 +0700

    asmt

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,10 @@ export default function Home() {
     <>
     <h1> หิวข้าว </h1>
     <div className="text-3xl">
-          SE66 Team20 ไก่ทอด
+          SE66 Team20 ไก่ทอด 
+          Jenkins server + Docker server
+          66026167 
+          66022826
     </div>
     </>
   );
