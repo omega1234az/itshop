804a81fcb37883385e6b259d0b293b2848ddf0f2
omega1234az <supakit0920@gmail.com>
2025-01-09 13:38:08 +0700

    red

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <>
     <h1> หิวข้าว </h1>
+    <div className="bg-red-500"> SE66 Team20 ไก่ทอด </div>
     <div className="text-3xl">
-          SE66 Team20 ไก่ทอด 
           Jenkins server + Docker server
           66026167 
           66022826
