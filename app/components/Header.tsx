9b89c237113716899fa588217439b33e0312685b
omega1234az <supakit0920@gmail.com>
2025-01-17 14:49:09 +0700

    เพิ่มหน้าหลัก

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link'
 export default function Header() {
     return <>
         <div className="mx-auto container grid grid-cols-10 text-center items-center gap-5">
-            <div className="bg-red-400 p-5">Logo</div>
-            <div className="font-bold">ติดต่อ</div>
+            <Link href="/" className="bg-red-400 p-5">Logo</Link>
+            <Link href="/contact" className="font-bold">ติดต่อ</Link>
             <input className="border border-black col-span-5 p-2 rounded-lg" placeholder="ค้นหา"></input>
             
                 <img src="/icon/search.png" alt="searth" className="cursor-pointer" />
