c59e21b7445ff0df7147d820241aae88f77c3b9e
omega1234az <supakit0920@gmail.com>
2025-02-07 16:25:58 +0700

    link api main

--- app/components/category.tsx
+++ app/components/category.tsx
@@ -1,13 +1,14 @@
 import Link from 'next/link'
 interface ItemProps {
+    id: number;
     name: string;
     img: string;
 }
 
-export default function Category({ name, img }: ItemProps) {
+export default function Category({ name, img ,id}: ItemProps) {
     return <>
         <button className="bg-gray-400 hover:bg-gray-500 w-full justify-items-center rounded-lg  h-36">
-        <Link href={"/productlist"}>
+        <Link href={"/productlist/"+id}>
                 <img src={img} alt="" className="h-16  object-cover mx-auto" />
                 <p className="mt-2 text-center">{name}</p>
                 </Link>
